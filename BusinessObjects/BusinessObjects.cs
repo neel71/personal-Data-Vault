d3b41ba4eacc5d8674fd23a091d8e823c4e62b19
neel71 <40604432+neel71@users.noreply.github.com>
2018-06-28 00:28:19 +0600

    Add files via upload

---
+++ BusinessObjects/BusinessObjects.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects
+{
+    public class BusinessObjects
+    {
+       // User Registration
+
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public string gender { get; set; }
+        public string loginid { get; set; }
+        public string password { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public string date { get; set; }
+        public string address { get; set; }
+        public string time { get; set; }
+        public int  admin_check { get; set; }
+        public int active { get; set; }
+        public int role { get; set; }
+        //**********************************
+
+
+    }
+}
