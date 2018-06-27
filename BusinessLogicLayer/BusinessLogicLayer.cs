8d890db82cd2967d2e8b6dcea22b66215c854b79
neel71 <40604432+neel71@users.noreply.github.com>
2018-06-28 00:27:19 +0600

    Add files via upload

---
+++ BusinessLogicLayer/BusinessLogicLayer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using BusinessObjects;
+using DataAccessLayer;
+namespace BusinessLogicLayer
+{
+    public class BusinessLogicLayer
+    {
+        DataAccessLayer.DataAccessLayer objdl = new DataAccessLayer.DataAccessLayer();
+        BusinessObjects.BusinessObjects objbo = new BusinessObjects.BusinessObjects();
+
+        #region  User Reg
+        public int user_reg(BusinessObjects.BusinessObjects objbo)
+        {
+            
+            int i = objdl.user_reg(objbo);
+
+            return i;
+        }
+        #endregion
+
+        //#region View User
+        //public DataSet Viewuser()
+        //{
+        //    DataSet ds = objdl.Viewuser();
+        //    return ds;
+        //}
+        //#endregion
+
+        #region View User Approved
+        public DataSet Viewuser()
+        {
+            DataSet ds = objdl.ViewuserApproved();
+            return ds;
+        }
+        #endregion
+
+        #region User Login
+        public int user_login(BusinessObjects.BusinessObjects objbo)
+        {
+            int i = objdl.user_login(objbo);
+            return i;
+        }
+        #endregion
+    }
+}
